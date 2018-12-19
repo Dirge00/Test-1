176a63acff99ca24929708526aec7d79808d2102
Dirge00 <44958265+Dirge00@users.noreply.github.com>
2018-12-19 15:09:54 +0500

    Update CalculatorAppDavydovTest2.java

--- RGR/CalculatorAppDavydovTest2.java
+++ RGR/CalculatorAppDavydovTest2.java
@@ -4,16 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculatorAppDavydovTest2 {
+public class CalculatorAppKalaevTest2 {
 
 	@Test
 	public void test() throws InterruptedException {
 		Calculator c = new Calculator(); //create instance of the calculator
-		c.textField1.setText("4"); //set test in the text field
-		c.textField2.setText("5"); //set test in the text field
-		c.textField3.setText("6"); //set test in the text field
+		c.textField1.setText("10"); //set test in the text field
+		c.textField2.setText("11"); //set test in the text field
+		c.textField3.setText("12"); //set test in the text field
 		c.countB.doClick(); //do click on the button
-		assertEquals("2", c.textField4.getText()); //check results
+		assertEquals("1", c.textField4.getText()); //check results
 		
 	}
 
